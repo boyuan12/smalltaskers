dc6a5b240c9f26faccf3b335e1aa878f888d46ce
Boyuan Liu <boyuanliu6@yahoo.com>
2021-05-19 21:50:23 -0700

    added payout feature

--- paypal/urls.py
+++ paypal/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("deposit/", views.accept_payment_view),
-    path("payment-success/", views.payment_success)
+    path("payment-success/", views.payment_success),
+    path("withdraw/", views.paypal_payout_view)
 ]
--- paypal/views.py
+++ paypal/views.py
@@ -1,7 +1,82 @@
 from django.http.response import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from paypalpayoutssdk.core import PayPalHttpClient, SandboxEnvironment
+from paypalpayoutssdk.payouts import PayoutsPostRequest
+from paypalhttp import HttpError
+from paypalhttp.encoder import Encoder
+from paypalhttp.serializers.json_serializer import Json
+import os
 
+
+# Creating Access Token for Sandbox
+client_id = os.getenv("PAYPAL_CLIENT_ID")
+client_secret = os.getenv("PAYPAL_CLIENT_SECRET")
+# Creating an environment
+environment = SandboxEnvironment(client_id=client_id, client_secret=client_secret)
+client = PayPalHttpClient(environment)
+
+
+import json
+import random
+import string
+from paypal_client import PayPalClient
+from paypalpayoutssdk.payouts import PayoutsPostRequest
+from paypalhttp.serializers.json_serializer import Json
+from paypalhttp.http_error import HttpError
+from paypalhttp.encoder import Encoder
+
+class CreatePayouts(PayPalClient):
+
+    """ Creates a payout batch with 5 payout items
+    Calls the create batch api (POST - /v1/payments/payouts)
+    A maximum of 15000 payout items are supported in a single batch request"""
+    @staticmethod
+    def build_request_body(include_validation_failure = False):
+        senderBatchId = str(''.join(random.sample(
+            string.ascii_uppercase + string.digits, k=7)))
+        amount = "1.0.0" if include_validation_failure else "1.00"
+        return \
+            {
+                "sender_batch_header": {
+                    "recipient_type": "EMAIL",
+                    "email_message": "SDK payouts test txn",
+                    "note": "Enjoy your Payout!!",
+                    "sender_batch_id": senderBatchId,
+                    "email_subject": "This is a test transaction from SDK"
+                },
+                "items": [{
+                    "note": "Your 1$ Payout!",
+                    "amount": {
+                        "currency": "USD",
+                        "value": amount
+                    },
+                    "receiver": "boyuanliu6@yahoo.com",
+                    "sender_item_id": "Test_txn_1"
+                }]
+            }
+
+    def create_payouts(self, debug=False):
+        request = PayoutsPostRequest()
+        request.request_body(self.build_request_body(False))
+        response = self.client.execute(request)
+
+        if debug:
+            print("Status Code: ", response.status_code)
+            print("Payout Batch ID: " +
+                  response.result.batch_header.payout_batch_id)
+            print("Payout Batch Status: " +
+                  response.result.batch_header.batch_status)
+            print("Links: ")
+            for link in response.result.links:
+                print('\t{}: {}\tCall Type: {}'.format(
+                    link.rel, link.href, link.method))
+
+            # To toggle print the whole body comment/uncomment the below line
+            #json_data = self.object_to_json(response.result)
+            #print "json_data: ", json.dumps(json_data, indent=4)
+
+        return response
 
 # Create your views here.
 def accept_payment_view(request):
@@ -17,4 +92,7 @@ def payment_success(request):
 
         return JsonResponse({"success": True})
 
+def paypal_payout_view(request):
+    create_response = CreatePayouts().create_payouts(True)
+    return JsonResponse({"status code": create_response.status_code})
 
