5459e3bcc7876a5fabb5faccc5d3307b33a5b4a6
Boyuan Liu <boyuanliu6@yahoo.com>
2021-06-26 18:47:03 -0700

    added drag and drop file upload, generate link and store in s3

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("post-job/", views.post_job)
+    path("", views.index),
+    path("post-job/", views.post_job),
+    path("task/<int:task_id>/", views.view_task),
+    path("api/upload/", views.file_upload_backend),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,12 +1,42 @@
+from django.http.response import HttpResponse, JsonResponse
 from django.shortcuts import redirect, render
 from .models import Job
+from paypal.models import UserFund
+from django.views.decorators.csrf import csrf_exempt
+import boto3
+import os
+import pathlib
+
+s3 = boto3.resource("s3", aws_access_key_id=os.getenv("S3_ACCESS_KEY_ID"), aws_secret_access_key=os.getenv("S3_SECRET_ACCESS_KEY_ID"))
+
 
 # Create your views here.
 def index(request):
-    return render(request, "dashboard/index.html")
+    jobs = Job.objects.all()
+    return render(request, "dashboard/index.html", {
+        "jobs": jobs
+    })
 
 def post_job(request):
     if request.method == "POST":
+        uf = UserFund.objects.get(user=request.user)
+        if uf.fund < float(request.POST["price"]) * int(request.POST["quantity"]):
+            return HttpResponse("Please deposit more money")
         Job(user=request.user, title=request.POST["title"], description=request.POST["description"], price=float(request.POST["price"]), quantity=int(request.POST["quantity"])).save()
         return redirect("/")
     return render(request, "dashboard/post-job.html")
+
+def view_task(request, task_id):
+    task = Job.objects.get(id=task_id)
+    return render(request, "dashboard/task.html", {
+        "task": task
+    })
+
+@csrf_exempt
+def file_upload_backend(request):
+    import string
+    import random
+    name = "".join([random.choice(string.ascii_letters + string.digits) for i in range(10)])
+    s3.Bucket('smalltasker').put_object(Key=f"{name}{pathlib.Path(request.FILES['file'].name).suffix}", Body=request.FILES["file"], ACL="public-read")
+    return JsonResponse({"url": f"https://smalltasker.s3.us-west-1.amazonaws.com/{name}{pathlib.Path(request.FILES['file'].name).suffix}"})
+
